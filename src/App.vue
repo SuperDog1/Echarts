f2a06450fec367a056de83aa5a9c14dddd6144eb
zhufuge <zhufuge@outlook.com>
2018-05-03 23:11:49 +0800

    start to write vue

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header><h1>双一流大学</h1></el-header>
+      <el-container id="main">
+        <el-aside width="260px"><Aside/></el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Aside from './components/Aside'
+
+export default {
+  name: 'App',
+  components: {
+    Aside
+  }
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    height: 100%;
+  }
+  .el-header {
+    color: #333;
+    border-bottom: 1px solid #eee;
+    text-align: center;
+  }
+  .el-aside {
+    position: fixed;
+    top: 60px;
+    bottom: 0;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+
+  .el-main {
+    margin-left: 260px;
+    color: #333;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+</style>
---
+++ src/components/Aside.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="aside">
+    <div class="title">省份</div>
+    <ul id="list">
+      <li v-for="item in province" :key="item">
+        <a href="">{{ item }}</a>
+      </li>
+    </ul>
+  </div>
+ </template>
+<script>
+import province from './province.json'
+export default {
+  name: 'Aside',
+  data () {
+    return {
+      province: province.data
+    }
+  }
+}
+</script>
+<style scoped>
+  #aside {
+    overflow: hidden;
+  }
+  .title {
+    border: 1px solid #eee;
+    border-right: none;
+    padding: 16px 46px;
+    font-size: 18px;
+    font-weight: 600;
+    color: #444;
+    box-shadow: 0 4px 4px 0 rgba(0,0,0,0.1);
+    position: fixed;
+    top: 60px;
+    width: 150px;
+    background: white;
+  }
+  ul {
+    margin-top: 80px;
+  }
+  li {
+    list-style-type: none;
+    margin: 8px;
+  }
+  li > a {
+    text-decoration: none;
+    color: #666;
+  }
+  li > a:hover {
+    color: #409eff;
+  }
+</style>
