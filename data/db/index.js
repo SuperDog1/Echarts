9e9e02c103b995074b50348aa5fac2cb75a50417
zhufuge <zhufuge@outlook.com>
2018-05-25 14:16:00 +0800

    update db level

--- data/db/index.js
+++ data/db/index.js
@@ -7,29 +7,40 @@ const readdir = promisify(fs.readdir)
 
 const connect = require('./connect')
 
-const result = connect(async function(table) {
-  const dirPath = path.join(__dirname, '../college')
-  const dir = await readdir(dirPath)
-  for (let f of dir) {
-    const { data } = JSON.parse(await readFile(path.join(dirPath, f), 'utf8'))
-    if (data[0]) {
-      const college = {
-        id: data[0].college_id,
-        name: data[0].name,
-        type: data[0].type,
-        addr: data[0].addr,
-        email: data[0].email,
-        brief_introduction: data[0].brief_introduction,
-        code: data[0].code,
-        recruit_website: data[0].recruit_website,
-        recruit_tel: data[0].recruit_tel,
-        province: data[0].province,
-        logo: data[0].logo,
-        tuition_fee: data[0].tuition_fee,
-        property: data[0].property,
-        member_ship: data[0].member_ship,
+function insert() {
+  const result = connect(async function(table) {
+    const dirPath = path.join(__dirname, '../college')
+    const dir = await readdir(dirPath)
+    for (let f of dir) {
+      const { data } = JSON.parse(await readFile(path.join(dirPath, f), 'utf8'))
+      if (data[0]) {
+        const college = {
+          id: data[0].college_id,
+          name: data[0].name,
+          type: data[0].type,
+          addr: data[0].addr,
+          email: data[0].email,
+          brief_introduction: data[0].brief_introduction,
+          code: data[0].code,
+          recruit_website: data[0].recruit_website,
+          recruit_tel: data[0].recruit_tel,
+          province: data[0].province,
+          logo: data[0].logo,
+          tuition_fee: data[0].tuition_fee,
+          property: data[0].property,
+          member_ship: data[0].member_ship,
+        }
+        table.insert(college)
       }
-      table.insert(college)
     }
-  }
-})
+  })
+}
+function update() {
+  const result = connect(async function(table) {
+    const data = JSON.parse(await readFile(path.join(__dirname, '../college-rank.json'), 'utf8'))
+    for (let line of data) {
+      table.update([{level: parseInt(line.rank, 10)}, {name: line.name}])
+    }
+  })
+}
+update()
